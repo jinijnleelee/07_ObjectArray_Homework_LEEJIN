d3bd35391ab5032cfb571a5486dff262ba7e59bd
LEE JINYOUNG <jinijnleelee@gmail.com>
2022-12-20 15:20:03 +0900

    객체배열

---
+++ src/com/hw1/run/Run.java
@@ -0,0 +1,45 @@
+package com.hw1.run;
+
+import java.util.Arrays;
+
+import com.hw1.model.vo.Employee;
+
+
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee[] emp = new Employee[3];
+		emp[0] = new Employee();
+		emp[1] = new Employee(1,"홍길동",19,'m',"01022223333","서울잠실");
+		emp[2] = new Employee(2,"강말순","교육부","강사",20,'F',1000000,0.01,
+				"01011112222","서울잠실");
+		
+		
+		
+		
+
+	
+//		1. 3개의 생성자를 사용하여 3명의 사원 정보를 배열로 생성한 후 출력 해보고
+//		2. 값이 없는 필드에 각각 값을 입력해 넣어 출력.
+//		3. 직원 각각의 보너스가 적용된 연봉을 계산하여 출력하고
+//		4. 총 직원 연봉의 평균을 구하여 출력
+		
+		//객체배열을 크기 3으로 할당 한 뒤 3개의 생성자를 각각 생성하여
+		//객체 생성 후 출력
+		
+		
+	}
+}
+
+		
+		
+		
+		
+
+		
+		
+
+
